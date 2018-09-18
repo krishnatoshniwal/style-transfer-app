2bc36c1e17c4073f0181ecc76568871fdf52d5e6
Krishna Toshniwal <krish9605@gmail.com>
2018-09-18 00:17:02 -0400

    Corrected setup script and tested building src, works!

--- setup_script.sh
+++ setup_script.sh
@@ -1,4 +1,4 @@
-cd arbitrary_style_transfer
+cd arbitrary_style_transfer_py
 wget https://cmu.box.com/shared/static/diaquzqnmae38rjcqsqbqf45fe7ptuq6.zip 
 unzip diaquzqnmae38rjcqsqbqf45fe7ptuq6.zip
 rm  diaquzqnmae38rjcqsqbqf45fe7ptuq6.zip
