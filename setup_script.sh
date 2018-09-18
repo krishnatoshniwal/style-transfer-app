357dd66be4eef0ac46f9e7f1516d29819d272e25
Krishna Toshniwal <krish9605@gmail.com>
2018-09-17 21:35:40 -0400

    Added setup scripts and requirements for python environment

---
+++ setup_script.sh
@@ -0,0 +1,4 @@
+cd arbitrary_style_transfer
+wget https://cmu.box.com/shared/static/diaquzqnmae38rjcqsqbqf45fe7ptuq6.zip 
+unzip diaquzqnmae38rjcqsqbqf45fe7ptuq6.zip
+rm  diaquzqnmae38rjcqsqbqf45fe7ptuq6.zip
